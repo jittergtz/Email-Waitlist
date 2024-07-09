874dc7028d7565bf0e097549595130c45bf385a7
Sandro <sandro.gantze@icloud.com>
2024-07-10 00:58:10 +0200

    change footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,13 +2,14 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div className='h-36 w-full p-5 bg-zinc-950 border-t border-zinc-700'>
+    <div className='h-20 w-full p-5 bg-zinc-950 border-t border-zinc-700'>
             <div className='flex h-full items-center justify-center'>
             <div className='max-w-5xl text-sm w-full'>
-               <div className='flex justify-between text-zinc-500  gap-2'>
+               <div className='flex items-center justify-between text-zinc-500  gap-2'>
                 <div>
-                <p>Footer</p>
-                <p>More footer content</p>
+                <p>
+                © Your company name, all rights reserved
+                </p>
                 </div>
          
                 <div>
